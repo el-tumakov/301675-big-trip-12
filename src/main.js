958719b195324ecbae58d8321a0de4acd82ef610
Егор Тумаков <el.tumakov@gmail.com>
2020-08-17 13:40:49 +0300

    Добавляет презентер отрисовки ивентов

--- src/main.js
+++ src/main.js
@@ -3,14 +3,9 @@ import TripPriceView from "./view/trip-price.js";
 import TitleH2View from "./view/title-h2.js";
 import SiteMenuView from "./view/site-menu.js";
 import FilterView from "./view/filter.js";
-import SortView from "./view/sort.js";
-import TripDaysView from "./view/trip-days.js";
-import DayView from "./view/day.js";
-import EventFormView from "./view/event-form.js";
-import EventPointView from "./view/event-point.js";
+import TripPresenter from "./presenter/trip.js";
 import {generateEventPoint} from "./mock/event-point.js";
-import {render, RenderPosition, replace} from "./utils/render.js";
-import {getUniqueDates} from "./utils/specific.js";
+import {render, RenderPosition} from "./utils/render.js";
 
 const EVENTS_COUNT = 25;
 
@@ -44,48 +39,7 @@ render(
 );
 render(tripControlsElement, new FilterView(), BEFOREEND);
 
-const mainElement = document.querySelector(`.page-main`);
-const tripEventsElement = mainElement.querySelector(`.trip-events`);
+const tripEventsElement = document.querySelector(`.trip-events`);
+const tripPresenter = new TripPresenter(tripEventsElement);
 
-render(tripEventsElement, new SortView(), BEFOREEND);
-render(tripEventsElement, new TripDaysView(), BEFOREEND);
-
-const tripDaysElement = mainElement.querySelector(`.trip-days`);
-
-getUniqueDates(events).forEach((item, index) => {
-  render(tripDaysElement, new DayView(item, index), BEFOREEND);
-});
-
-const renderEvent = (eventsListElement, event) => {
-  const eventComponent = new EventPointView(event);
-  const eventEditComponent = new EventFormView(event);
-
-  const replaceEventToForm = () => {
-    replace(eventEditComponent, eventComponent);
-  };
-
-  const replaceFormToEvent = () => {
-    replace(eventComponent, eventEditComponent);
-  };
-
-  eventComponent.setEditClickHandler(() => {
-    replaceEventToForm();
-  });
-
-  eventEditComponent.setFormSubmitHandler(() => {
-    replaceFormToEvent();
-  });
-
-  render(eventsListElement, eventComponent, RenderPosition.BEFOREEND);
-};
-
-for (let i = 0; i < EVENTS_COUNT; i++) {
-  const {time} = events[i];
-  const timeISO = time.start.toISOString().slice(0, -14);
-  const timeElement = tripDaysElement
-    .querySelector(`.day .day__date[datetime="${timeISO}"]`);
-  const dayElement = timeElement.closest(`.day`);
-  const pointsListElement = dayElement.querySelector(`.trip-events__list`);
-
-  renderEvent(pointsListElement, events[i]);
-}
+tripPresenter.init(sortEvents);
