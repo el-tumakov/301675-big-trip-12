39dede46d485641bb386d27b2ea027db4e279168
Егор Тумаков <el.tumakov@gmail.com>
2020-08-26 09:45:53 +0300

    Добавляет класс для модели ивентов
    - создает класс Observer и унаследует от него модель ивентов
    - объявляет методы для записи, получения и обновления всех ивентов

---
+++ src/model/events.js
@@ -0,0 +1,32 @@
+import Observer from "../utils/observer.js";
+
+export default class Events extends Observer {
+  constructor() {
+    super();
+    this._events = [];
+  }
+
+  setEvents(events) {
+    this._events = events.slice();
+  }
+
+  getEvents() {
+    return this._events;
+  }
+
+  updateEvents(updateType, update) {
+    const index = this._events.findIndex((event) => event.id === update.id);
+
+    if (index === -1) {
+      throw new Error(`Can't update unexisting event`);
+    }
+
+    this._events = [
+      ...this._events.slice(0, index),
+      update,
+      ...this._events.slice(index + 1)
+    ];
+
+    this._notify(updateType, update);
+  }
+}
